56a5472f9c3270dcb6191b88bf17472d4dc4974d
smshasan <smshasan100@gmail.com>
2021-08-18 22:52:52 +0600

    config update

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,7 +8,7 @@ const sendToken = (user, statusCode, res) => {
     const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
+        ).toGMTString(),
         httpOnly: true
     }
 
